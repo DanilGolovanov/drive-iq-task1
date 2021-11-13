ba05ddb6edec7ea70fd0ed240756c168c291b0cf
DanilGolovanov <golovanov.da1@gmail.com>
2021-11-13 12:48:31 +1100

    added loading all objects from the API endpoint to the console. Also added consistent indentation and axios + react-bootstrap packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import axios from 'axios'
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+
+	async function fetchUnis() {
+		const response = await axios.get('http://universities.hipolabs.com/search?country=Australia')
+		console.log(response.data)
+	}
+
+	return (
+		<div className="App">
+			<button onClick={fetchUnis}>LOAD</button>
+			<h1>Hello World</h1>
+		</div>
+  	);
 }
 
 export default App;
