24d8c5ad68476fde602d8b24a8a68479d44b4e71
DanilGolovanov <golovanov.da1@gmail.com>
2021-11-13 16:33:45 +1100

    headers can be loaded now and if they were not loaded then a custom message is displayed.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import InfoTable from './components/InfoTable'
 
 function App() {
-	const [entries, setEntries] = useState([])
+
+	const [entries, setEntries] = useState([]);
+	// separate state for headings to fetch them when page loads, so that 
+	// the empty table is displayed with headings that were fetched from 
+	// the API endpoint (better than just hard coding them)
+	const [headings, setHeadings] = useState([]);
+
+	// useEffect(() => {
+	// 	const response = axios.get('http://universities.hipolabs.com/search?country=Australia')
+	// 	setHeadings(response.data)
+	// 	console.log(headings)
+	// }, [headings])
 
 	async function fetchEntries() {
 		const response = await axios.get('http://universities.hipolabs.com/search?country=Australia')
 		setEntries(response.data)
+		console.log(entries)
 	}
 
 	return (
 		<div className="App">
 			<button onClick={fetchEntries}>LOAD</button>
-			<InfoTable entries={entries} />
-			<h1>Hello World</h1>
+			{entries.length != 0
+				? <InfoTable entries={entries} />
+				: <h1>Entries were not loaded yet.</h1>
+			}
 		</div>
   	);
 }
--- src/components/InfoTable.js
+++ src/components/InfoTable.js
@@ -2,34 +2,33 @@ import React from 'react'
 import Table from 'react-bootstrap/Table'
 
 export default function InfoTable(props) {
+
+    function renderHeader() {
+        let headings = [];
+
+        try {
+            let keys = Object.keys(props.entries[0]);
+            for (let i = 0; i < keys.length; i++) 
+                headings.push(<th key={keys[i]}>{keys[i]}</th>);
+        }
+        catch(e) {
+            console.log(e)
+        }
+        finally {
+            console.log(headings)
+            return headings;
+        }
+    }
+    
     return (
         <Table striped bordered hover>
             <thead>
                 <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
+                    {renderHeader()}
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td colSpan="2">Larry the Bird</td>
-                    <td>@twitter</td>
-                </tr>
+                {}
             </tbody>
         </Table>
     )
